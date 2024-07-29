243cfeefe12c7c1183146b1feb22a7fc9fdb0b73
daikj <tyutyunniksolomia@gmail.com>
2024-07-29 12:58:32 +0300

    Revert "Merge branch 'develop'"
    This reverts commit 35ef011a664205a5839d1bcd82dbd1ad317b2dc6, reversing
    changes made to 996a4febee921d655fa338ef170bc593c1a024bc.

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("task_manager.urls", namespace="task_manager")),
-    path("accounts/", include("django.contrib.auth.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+]
