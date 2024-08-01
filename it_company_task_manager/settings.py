811b3f81d0c4682b0681d7729ab8251c559d3ae3
daikj <tyutyunniksolomia@gmail.com>
2024-08-01 15:36:25 +0300

    add it-company-task-manager-1qnh.onrender.com to ALLOWED_HOSTS

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "it-company-task-manager-1qnh.onrender.com"]
 
 # Application definition
 
